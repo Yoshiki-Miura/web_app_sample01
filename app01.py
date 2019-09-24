4ef32d8e3f71a19849c476cf89cfedf12e5609ee
yoshiki-san <yy.bb.tf@gmail.com>
2019-09-24 15:56:06 +0900

    おみくじできた

--- app01.py
+++ app01.py
@@ -1,3 +1,4 @@
+import numpy as np
 from flask import Flask
 
 app = Flask(__name__)
@@ -16,6 +17,17 @@ def goodbye():
 @app.route("/add/<a>/<b>")
 def add(a, b):
     return str(int(a) + int(b))
+
+
+@app.route("/omikuji")
+def omikuji():
+    r = np.random.random()
+    if r < 0.5:
+        return "凶"
+    elif 0.5 <= r and r < 0.8:
+        return "吉"
+    else:
+        return "大吉"
 
 
 if __name__ == '__main__':
